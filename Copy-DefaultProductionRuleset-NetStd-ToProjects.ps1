aaa235cd4d32c00e8e04c766b22ec17a24231708
Michael Prochaska <mprochaska@live.com>
2019-01-23 20:33:20 +0000

    fix errant close XML comment in ruleset files

--- Copy-DefaultProductionRuleset-NetStd-ToProjects.ps1
+++ Copy-DefaultProductionRuleset-NetStd-ToProjects.ps1
@@ -2,5 +2,4 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Continue'
 
 Copy-Item -Path .\build\BuildFiles\Default.Production.NetStd.FxCop.15.0.WithSonarLint.ruleset -Destination .\source\Landorphan.Common\Landorphan.Common.NetStd.ruleset
-Copy-Item -Path .\build\BuildFiles\Default.Production.NetStd.FxCop.15.0.WithSonarLint.ruleset -Destination .\test\Landorphan.TestUtilities.NoIoc\Landorphan.TestUtilities.NoIoc.NetStd.ruleset
  
